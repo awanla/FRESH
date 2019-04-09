3e09cccfe9b64c03d74fbf20e4b8b5bb9a966d60
awanla <48355798+awanla@users.noreply.github.com>
2019-04-09 15:27:22 +0800

    改冬冬

--- js/all.js
+++ js/all.js
@@ -2,11 +2,14 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         //options here
         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', '5thPage'],
-        menu: '#myMenu'
+        menu: '#myMenu',
+        scrollingSpeed: 1000,
+        autoScrolling: false,
+        fitToSection: false
     });
     $('.menu_icon').click(function (event) {
         /* Act on the event */
         $('body').toggleClass('open');
     });
-
+    var rellax = new Rellax('.rellax');
 });
