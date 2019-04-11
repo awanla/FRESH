454d88568abdd04cdbf0ba61f561ced8e1d77979
awanla <48355798+awanla@users.noreply.github.com>
2019-04-11 11:36:21 +0800

    4,5頁面上傳

--- js/all.js
+++ js/all.js
@@ -12,6 +12,21 @@ $(document).ready(function () {
         $('body').toggleClass('open');
     });
     var rellax = new Rellax('.rellax');
+
+
+    // 當 input 是 focus 的狀態下
+    $(".js-input").focus(function () {
+        // 找到 .label-text 元素並加上 .is-active
+        $(this).parent().find(".label-text").addClass("is-active");
+    })
+    // 當 input 是 blur 的狀態下
+    $(".js-input").blur(function () {
+        // 如果裡面沒有內容
+        if ($(this).val().length == 0) {
+            // 找到 .label-text 元素並移除 .is-active
+            $(this).parent().find(".label-text").removeClass("is-active");
+        }
+    })
     
     $(window).resize(function () {
     var windowWidth = $(window).width();
@@ -36,7 +51,6 @@ $(document).ready(function () {
             $('.tp').removeClass('all');
         }
     
-       
     });
     
    
